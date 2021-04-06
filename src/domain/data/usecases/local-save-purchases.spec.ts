08d72f51048b676cc3887f18e277008e5e22aee4
RafaDraw <www.rafadraw@gmail.com>
2021-04-06 20:03:44 -0300

    ensure LocalSavePurchases delete old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,20 +1,37 @@
 class LocalSavePurchases {
 	constructor(private readonly cacheStore: CacheStore) {
 	}
+
+	async save(): Promise<any> {
+		this.cacheStore.delete();
+	}
 }
 
 interface CacheStore {
-
+	delete: () => void;
 }
 
 class CacheStoreSpy implements CacheStore {
 	deleteCallsCount = 0
+
+	delete(): void {
+		this.deleteCallsCount++
+	}
 }
 
 describe('#LocalSavePurchases', () => {
 	test('Should not delete cache on sut.init', () => {
 		const cacheStore = new CacheStoreSpy();
+		new LocalSavePurchases(cacheStore);
+
+		expect(cacheStore.deleteCallsCount).toBe(0);
+	});
+
+	test('should delete old cache on sut.save', () => {
+		const cacheStore = new CacheStoreSpy();
 		const sut = new LocalSavePurchases(cacheStore);
-		expect(cacheStore.deleteCallsCount).toBe(0)
-	})
+		sut.save();
+
+		expect(cacheStore.deleteCallsCount).toBe(1);
+	});
 })
