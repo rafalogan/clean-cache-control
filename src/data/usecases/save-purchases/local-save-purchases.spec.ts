2e1dfdefedb3f9028a7288c9e0709eefca7990bd
RafaDraw <www.rafadraw@gmail.com>
2021-04-07 17:31:56 -0300

    change index dor domain/usecases

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -1,6 +1,6 @@
 import LocalSavePurchases from '@/data/usecases/save-purchases/local-save-purchases';
 import { CacheStore } from '@/data/protocols/cache';
-import { SavePurchases } from '@/domain';
+import { SavePurchases } from '@/domain/usecases';
 
 class CacheStoreSpy implements CacheStore {
 	deleteCallsCount = 0;
